2353e2306336e3f6d3b9f7395cb12cfcc229d3e6
emirgocer43-hash <emirgocer43@icloud.com>
2026-01-20 14:20:14 +0300

    Clean up plot saving logic

--- analyze_data.py
+++ analyze_data.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Read CSV data
 data = pd.read_csv("data.csv")
 
-
+# Create plot
 plt.plot(data.iloc[:, 0], data.iloc[:, 1])
 plt.xlabel("X values")
 plt.ylabel("Y values")
 plt.title("Simple Data Plot")
 plt.grid(True)
 
-
-plt.savefig("plot.png")
+# Save output
+output_name = "output_plot.png"
+plt.savefig(output_name)
 plt.show()
+
+print(f"Plot saved as {output_name}")
